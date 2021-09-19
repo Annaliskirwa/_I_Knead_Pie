feab046cf9074fcc7946ca9118cdcad790212531
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-19 15:24:34 +0300

    It will add a hover effect on the varieties images

---
+++ script.js
@@ -0,0 +1,62 @@
+//user interface logic
+$(document).ready(function(){
+    //varieties scripts
+    $("#col1").hover(function() {
+        $("#text1").fadeTo("slow",1);
+        $("#pic1").fadeTo("slow",0.4);
+        },function() {
+          $("#text1").fadeTo("slow",0);
+          $("#pic1").fadeTo("slow",1);
+        });
+    
+        $("#col2").hover(function() {
+            $("#text2").fadeTo("slow",1);
+            $("#pic2").fadeTo("slow",0.4);
+            },function() {
+              $("#text2").fadeTo("slow",0);
+              $("#pic2").fadeTo("slow",1);
+            });
+    
+         $("#col3").hover(function() {
+            $("#text3").fadeTo("slow",1);
+             $("#pic3").fadeTo("slow",0.4);
+             },function() {
+                 $("#text3").fadeTo("slow",0);
+                $("#pic3").fadeTo("slow",1);
+            });
+            $("#col4").hover(function() {
+                $("#text4").fadeTo("slow",1);
+                $("#pic4").fadeTo("slow",0.4);
+                },function() {
+                  $("#text4").fadeTo("slow",0);
+                  $("#pic4").fadeTo("slow",1);
+                });
+            $("#col5").hover(function() {
+                $("#text5").fadeTo("slow",1);
+                $("#pic5").fadeTo("slow",0.4);
+                },function() {
+                    $("#text5").fadeTo("slow",0);
+                     $("#pic5").fadeTo("slow",1);
+                 });
+                 $("#col6").hover(function() {
+                    $("#text6").fadeTo("slow",1);
+                    $("#pic6").fadeTo("slow",0.4);
+                    },function() {
+                      $("#text6").fadeTo("slow",0);
+                      $("#pic6").fadeTo("slow",1);
+                    });
+                    $("#col7").hover(function() {
+                        $("#text7").fadeTo("slow",1);
+                        $("#pic7").fadeTo("slow",0.4);
+                        },function() {
+                          $("#text7").fadeTo("slow",0);
+                          $("#pic7").fadeTo("slow",1);
+                        });
+                        $("#col8").hover(function() {
+                            $("#text8").fadeTo("slow",1);
+                            $("#pic8").fadeTo("slow",0.4);
+                            },function() {
+                              $("#text8").fadeTo("slow",0);
+                              $("#pic8").fadeTo("slow",1);
+                            });
+})

32da7411e02bce407199147c91206a9eb36e9895
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-20 10:12:47 +0300

    It will add a switch to ceck on psize and print on console

--- script.js
+++ script.js
@@ -76,32 +76,30 @@ function GetPizza(name,size, crust, topping,total){
 $(document).ready(function(){
     $("button.proceed").click(function(event){
         let pname = $(".name option:selected").val();
-        let psize = $(".name option:selected").val();
-        let pcrust = $("name option:selected").val();
+        let psize = $("#size option:selected").val();
+        let pcrust = $("#crust option:selected").val();
         let ptopping = [];
         $.each($("input[name='toppings']:checked"), function(){
             ptopping.push($(this).val());
         });
         console.log(ptopping.join(", "));
 
-        switch (psize){
+        switch(psize){
             case "0":
-                price = 0;
-                break;
+              price =0;
+            break;
             case "large":
-                price = 1200;
-                console.log(price);
-                break;
-            case "medium":
-                price = 850;
-                console.log(price);
-                break;
-            case "small":
-                price = 600;
-                console.log(price);
-                break;
-            default:
-                console.log("error")
-        }
+               price = 1200;
+               console.log("The price is "+price);
+             break;
+             case "medium":
+               price = 850;
+               console.log("The price is "+price);
+             break;
+             case "small":
+               price = 600;
+               console.log("The price is "+price);
+             break;
+           }
     });
 });

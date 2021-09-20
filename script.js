ad7e8d458bb62a22ee5521d68e8b792ff956e44c
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-20 10:54:06 +0300

    It will display the new order in a table

--- script.js
+++ script.js
@@ -196,6 +196,12 @@ $(document).ready(function(){
 
         checkoutTotal = checkoutTotal + total;
         console.log(checkoutTotal);
+
+        var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
+
+      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+      console.log(newOrder);
+      });
   
         });
 });

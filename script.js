2bed012d7c0cc83bc0cd667bf92dc03dddb562a5
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-20 10:50:59 +0300

    It will check the size of new pizza order

--- script.js
+++ script.js
@@ -153,5 +153,24 @@ $(document).ready(function(){
                 ptopping.push($(this).val());
             });
         console.log(ptopping.join(", "));
+
+        switch(psize){
+            case "0":
+              price =0;
+            break;
+            case "large":
+               price = 1200;
+               console.log(price);
+             break;
+             case "medium":
+               price = 850;
+               console.log("The price is "+price);
+             break;
+             case "small":
+               price = 600;
+               console.log(price);
+             default:
+               console.log("error"); 
+           }
         });
 });

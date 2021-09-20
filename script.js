b933625ebceecba3e41e004f90ca3fce82637f8f
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-20 13:13:13 +0300

    It will add animations onn scrolling

--- script.js
+++ script.js
@@ -250,3 +250,13 @@ $(document).ready(function(){
   
         });
 });
+//adding animations to the page
+$(document).ready(function(){
+    $(".nav-link").on("click", function(e){
+        e.preventDefault();
+        var hash = this.hash;
+        $("html, body").animate({scrollTop:$(hash).offset().top}, 1000, function(){
+            window.location.hash = hash
+        })
+    })
+});
